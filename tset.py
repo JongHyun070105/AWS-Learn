2caedf2ad878c7c7aec670e428071109e9469d0c
JongHyun070105 <bobi09361@gmail.com>
2025-04-01 19:38:26 +0900

    AWS Learn

--- tset.py
+++
@@ -1,7 +0,0 @@
-import boto3
-
-dynamodb = boto3.resource('Your SQL',region_name='ap-northeast-2',aws_access_key_id='Your AWS KEY',aws_secret_access_key='Your AWS KEY')
-
-table = dynamodb.Table('Your Table')
-data = {'id' : 00000 , 'name' : 'Your Name'}
-table.put_item(Item = data)
